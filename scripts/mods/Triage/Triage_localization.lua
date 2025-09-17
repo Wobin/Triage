e5e0f9d4e07ff37196fca42122e37d5aed288b42
Wobin <wobster@gmail.com>
2025-09-18 04:25:52 +1000

    zh-cn localisation

--- scripts/mods/Triage/Triage_localization.lua
+++ scripts/mods/Triage/Triage_localization.lua
@@ -1,5 +1,10 @@
 return {
+	mod_name = {
+		en = "Triage",
+		["zh-cn"] = "重伤警示",
+	},
 	mod_description = {
 		en = "Will highlight squad members who have one wound left due to corruption while wielding a med stimm",
+		["zh-cn"] = "当持有医疗针剂时，高亮显示因腐蚀而只剩一格生命值的队友",
 	},
 }
